a06093866cd4da6e88daaffd89365c172673bddd
satya sindhu <movva001@gmail.com>
2021-11-06 16:00:31 -0400

    modified client.js

---
+++ client.js
@@ -0,0 +1,16 @@
+// Client <---------
+const net = require("net");
+// establishes a connection with the game server
+const connect = function() {
+  const conn = net.createConnection({
+    host:'165.227.47.243', // IP address here,
+    port:50541 // PORT number here,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  return conn;
+};
+
+module.exports = {connect};
